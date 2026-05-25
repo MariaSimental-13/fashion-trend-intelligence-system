f1e01aea78d2a37093ceca56f33722999451393a
MariaSimental-13 <marysimental86@gmail.com>
2026-05-24 22:04:08 -0600

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -497,13 +497,13 @@ elif selected == "Estéticas":
 
     st.plotly_chart(fig2, use_container_width=True)
 elif selected == "Cultura":
-     IMG_SOFISMO    = img_to_base64("EL_SOFISMO_MODERNO.png")
-     IMG_MAQUIAVELO = img_to_base64("LA_ERA_DEL_MAQUIAVELISMO_EMOCIONAL.png")
-     IMG_CANSADOS   = img_to_base64("NOS_ESTÁN_CRIANDO_CANSADOS.png")
-     IMG_MEXI       = img_to_base64("Meximalismo.jpg")
-     IMG_CULTURA1   = img_to_base64("Cultura1.jpg")
-     IMG_MEXI3      = img_to_base64("Meximalismo3.jpg")
-     IMG_MEXI1      = img_to_base64("meximalismo1.jpg")
+    IMG_SOFISMO    = img_to_base64("EL_SOFISMO_MODERNO.png")
+    IMG_MAQUIAVELO = img_to_base64("LA_ERA_DEL_MAQUIAVELISMO_EMOCIONAL.png")
+    IMG_CANSADOS   = img_to_base64("NOS_ESTÁN_CRIANDO_CANSADOS.png")
+    IMG_MEXI       = img_to_base64("Meximalismo.jpg")
+    IMG_CULTURA1   = img_to_base64("Cultura1.jpg")
+    IMG_MEXI3      = img_to_base64("Meximalismo3.jpg")
+    IMG_MEXI1      = img_to_base64("meximalismo1.jpg")
 
      CULTURA_HTML = f"""
      <!DOCTYPE html>
